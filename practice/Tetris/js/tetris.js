d4aa82e345bea721753009d1626f9df18ccad78c
baeian <ddoory1103@gmail.com>
2022-07-17 12:44:46 +0900

    행열 구현및 점수표시

---
+++ practice/Tetris/js/tetris.js
@@ -0,0 +1,26 @@
+//DOM 선언
+const playground = document.querySelector(".playground > ul");
+
+//Setting
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
+
+//functions
+function init(){
+   
+}
+
+for (let i = 0; i < GAME_ROWS; i++) {
+   prependNewLine();
+}
+
+function prependNewLine() {
+   const li = document.createElement("li");
+   const ul = document.createElement("ul");
+   for (let j = 0; j < GAME_COLS; j++) {
+      const matrix = document.createElement("li");
+      ul.prepend(matrix);
+   }
+   li.prepend(ul)
+   playground.prepend(li)
+}
