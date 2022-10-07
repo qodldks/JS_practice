2220f29f28599d050b9ee192617f43cced4ab1fb
baeian <ddoory1103@gmail.com>
2022-10-07 21:22:20 +0900

    basic

---
+++ practice/Basic/buttoncontrol/index.js
@@ -0,0 +1,33 @@
+const plusButton = document.querySelector(".plus");
+const minusButton = document.querySelector(".minus");
+const numOutPut = document.querySelector(".valueNum");
+const stringInput = document.querySelector(".stringInput");
+const stringOutput = document.querySelector(".valueString");
+const colorInput = document.querySelector(".colorInput");
+const coloroutPut = document.querySelector(".valueColor");
+
+var color;
+var num = 0;
+var string = "null";
+numOutPut.innerHTML = num;
+stringOutput.innerHTML = string;
+
+plusButton.addEventListener("click", () => {
+   numOutPut.innerHTML = ++num;
+});
+
+minusButton.addEventListener("click", () => {
+   numOutPut.innerHTML = --num;
+});
+
+stringInput.addEventListener("change", () => {
+   string = stringInput.value;
+   if (!string) string = "Null";
+   stringOutput.innerHTML = string;
+});
+
+colorInput.addEventListener("change", () => {
+   color = colorInput.value;
+   coloroutPut.innerHTML=color;
+   coloroutPut.style.backgroundColor=color;
+});
