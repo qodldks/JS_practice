c26d793078107b01c17aa698728610bc19a8ad62
baeian <ddoory1103@gmail.com>
2022-10-15 23:25:33 +0900

    init

--- practice/Basic/buttoncontrol/index.js
+++ practice/Basic/buttoncontrol/index.js
@@ -4,7 +4,7 @@ const numOutPut = document.querySelector(".valueNum");
 const stringInput = document.querySelector(".stringInput");
 const stringOutput = document.querySelector(".valueString");
 const colorInput = document.querySelector(".colorInput");
-const coloroutPut = document.querySelector(".valueColor");
+const colorOutput = document.querySelector(".valueColor");
 
 var color;
 var num = 0;
@@ -13,21 +13,21 @@ numOutPut.innerHTML = num;
 stringOutput.innerHTML = string;
 
 plusButton.addEventListener("click", () => {
-   numOutPut.innerHTML = ++num;
+	numOutPut.innerHTML = ++num;
 });
 
 minusButton.addEventListener("click", () => {
-   numOutPut.innerHTML = --num;
+	numOutPut.innerHTML = --num;
 });
 
 stringInput.addEventListener("change", () => {
-   string = stringInput.value;
-   if (!string) string = "Null";
-   stringOutput.innerHTML = string;
+	string = stringInput.value;
+	if (!string) string = "Null";
+	stringOutput.innerHTML = string;
 });
 
 colorInput.addEventListener("change", () => {
-   color = colorInput.value;
-   coloroutPut.innerHTML=color;
-   coloroutPut.style.backgroundColor=color;
+	color = colorInput.value;
+	colorOutput.innerHTML = color;
+	colorOutput.style.backgroundColor = color;
 });
